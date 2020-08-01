da1d2df87dfa5e9b7d431c0122dba49a51d20355
Gideon Ahiadzi <60545561+horlali@users.noreply.github.com>
2020-08-01 20:34:47 +0000

    Update time_tracking_update.py

--- time_tracking_update.py
+++ time_tracking_update.py
@@ -6,7 +6,6 @@ Created on Sat Aug  1 13:47:15 2020
 """
 
 from datetime import datetime
-import csv
 
 # Customer and Task/Project Information
 customer_name = input('Enter Customer Name Here \n')
@@ -20,11 +19,9 @@ system_date = datetime.now().date()
 
 # Accepting User Input: Task Name, Start Time and End Time
 # This code takes the user start and end time as a string
-task_name = input('Enter task to be performed here \n')
-start_time = input('Enter start time in this format: HH:MM AM/PM: ')
-end_time = input('Enter end time in this format: HH:MM AM/PM: ')
-
-#time_format = '%I:%M %p'
+services = input('Enter task to be performed here \n')
+start_time = input('Enter start time in this format: HH:MM AM/PM: \n')
+end_time = input('Enter end time in this format: HH:MM AM/PM: \n')
 
 
 # Converting the Start and End time from string to Datetime by using the strptime() method
@@ -39,11 +36,12 @@ time_spent = round(((end_time - start_time).seconds) / 3600, 2)
 
 # Calculating the Amount of Money the User Earns for Work Done
 
-price_per_hour = 5            # amount in dollars
-amount_due = round((time_spent*price_per_hour))
+amount_charged = int(input('Enter the amount you charge for this service \n'))
+amount_due = round((time_spent*amount_charged))
 
 
 #Recording All Information Collected
+
 records = []
 
 # Convert from datetime format to string using datetime.strftime 
@@ -51,35 +49,34 @@ system_date = datetime.strftime(system_date, '%D')
 start_time = datetime.strftime(start_time, '%I:%M %p')
 end_time = datetime.strftime(end_time, '%I:%M %p')
 
+# Appending information to the records list
+
 records.append(customer_name)
 records.append(customer_contact)
 records.append(customer_location)
-records.append(task_name)
+records.append(services)
+records.append(amount_charged)
 records.append(system_date)
 records.append(start_time)
 records.append(end_time)
-records.append(time_spent)
+records.append(time_spent)      # time spent is measured in hours
 records.append(amount_due)
 
 
 # Passing the information collected into a csv file
+
+import csv
+
 with open('record_book.csv', mode = 'a') as record_book:
     record_writer = csv.writer(record_book, delimiter = ',')
     record_writer.writerow(records)
 
 
-# View the results 
-print ('\n') 
-print ('..'*40) 
-print ('Date : {}'.format(system_date)) 
-print ('Total hours spent on the work : {}'.format(time_spent)) 
-print ("Amount of money earned for the day : ${:0,.2f}".format(amount_due)) 
-print ('..'*40) 
-print ('Information written to records successfully') 
-
-
-#import time
-
-#The output stored in a file for future use
-
-
+# View the results
+print ('\n')
+print ('..'*50)
+print ('Date : {}'.format(system_date))
+print ('Total hours spent on the work : {}'.format(time_spent))
+print ("Amount of money earned for the day : ${:0,.2f}".format(amount_due))
+print ('..'*50)
+print ('Information written to records successfully')
